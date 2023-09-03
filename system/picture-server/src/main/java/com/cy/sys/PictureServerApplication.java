cf56ebccf6c8237c2d840d8f9d62b63abaa4ecdd
Lil-Kr <cxqybox@163.com>
2023-09-03 12:07:24 +0800

    change maven config

---
+++ system/picture-server/src/main/java/com/cy/sys/PictureServerApplication.java
@@ -0,0 +1,12 @@
+package com.cy.sys;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Lil-K
+ * @Date: 2023/9/3
+ * @Description:
+ */
+@SpringBootApplication
+public class PictureServerApplication {
+}
